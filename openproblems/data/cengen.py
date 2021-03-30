6b9d4235b36a6d6319e90d35823e2c3234d71c8e
Munfred <munfred@brandeis.edu>
2021-03-30 14:34:21 -0700

    fix lines being too long

--- openproblems/data/cengen.py
+++ openproblems/data/cengen.py
@@ -5,14 +5,16 @@ import scanpy as sc
 import scprep
 import tempfile
 
-URL = "https://github.com/Munfred/wormcells-data/releases/download/taylor2020/taylor2020.h5ad"
+URL = "https://github.com/Munfred/wormcells-data/"+ \
+"releases/download/taylor2020/taylor2020.h5ad"
 
 
 @utils.loader
 def load_cengen(test=False):
     """Download CeNGEN data from GitHub."""
     """To learn about how the data was generated visit www.cengen.org"""
-    """To learn about WormBase curation efforts for C. elegans single cell data visit https://wormbase.github.io/single-cell/"""
+    """To learn about WormBase curation efforts for C. elegans single cell 
+    data visit https://wormbase.github.io/single-cell/"""
     with tempfile.TemporaryDirectory() as tempdir:
         filepath = os.path.join(tempdir, "cengen.h5ad")
         scprep.io.download.download_url(URL, filepath)
