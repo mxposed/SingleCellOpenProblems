62ffeb49e17c28d399cc10356e5086d2f60e4fdc
Nikolay Markov <nikolai.markov@icloud.com>
2023-01-09 18:38:05 -0600

    Rename zebrafish_labels to zebrafish_labs dataset

--- openproblems/tasks/label_projection/datasets/zebrafish.py
+++ openproblems/tasks/label_projection/datasets/zebrafish.py
@@ -5,14 +5,14 @@ import numpy as np
 
 
 @dataset(
-    "Zebrafish (by labels)",
+    "Zebrafish (split by laboratory)",
     data_url=load_zebrafish.metadata["data_url"],
     data_reference=load_zebrafish.metadata["data_reference"],
     dataset_summary="90k cells from zebrafish embryos throughout the first day of "
     "development, with and without a knockout of chordin, an important developmental "
     "gene. Split into train/test by laboratory.",
 )
-def zebrafish_labels(test=False):
+def zebrafish_labs(test=False):
     adata = load_zebrafish(test=test)
     adata.obs["labels"] = adata.obs["cell_type"]
     adata.obs["batch"] = adata.obs["lab"]
